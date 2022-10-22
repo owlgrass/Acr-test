b33f1548ca01b992eeea143f6e0cb670b49b640e
owlgrass <32409699+owlgrass@users.noreply.github.com>
2022-10-22 21:27:35 +0800

    Add TopBar.vue
    Update TopBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
 	<div id="app">
+		<TopBar />
 		<router-view />
 	</div>
 </template>
+<script>
+import TopBar from '@/layout/TopBar'
+export default {
+	components: {
+		TopBar,
+	},
+}
+</script>
 
-<style></style>
+<style>
+@import './App.css';
+</style>
---
+++ src/layout/TopBar.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="top-bar">
+		<el-menu mode="horizontal">
+			<el-menu-item>
+				<router-link to="/about">About</router-link>
+			</el-menu-item>
+		</el-menu>
+	</div>
+</template>
+
+<style lang="scss">
+$--font-path: '~element-ui/lib/theme-chalk/fonts';
+@import '~element-ui/packages/theme-chalk/src/index';
+
+.top-bar {
+	max-width: $--md;
+	margin: 0 auto;
+}
+.el-menu--horizontal > .el-menu-item {
+	color: $--color-primary;
+}
+a {
+	text-decoration: inherit;
+}
+</style>
