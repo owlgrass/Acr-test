f37aad319f05b334592c6425efc06788c1f67f44
owlgrass <32409699+owlgrass@users.noreply.github.com>
2022-10-24 23:51:31 +0800

    Add unit tests for TodoView

---
+++ tests/unit/TodoView.spec.js
@@ -0,0 +1,32 @@
+import Vuex from 'vuex'
+import { mount, createLocalVue } from '@vue/test-utils'
+import store from '@/store'
+import TodoView from '@/views/HomeView/TodoView.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('TodoView.vue', () => {
+	it('renders todo list with 0 items', () => {
+		const wrapper = mount(TodoView, {
+			store,
+		})
+		expect(wrapper.text()).toMatch('No items')
+	})
+
+	it('renders todo list with 1 or more items', () => {
+		store.dispatch('add', {
+			content: 'this is the first item',
+			checked: true,
+		})
+		store.dispatch('add', {
+			content: 'this is the second item',
+			checked: true,
+		})
+		const wrapper = mount(TodoView, {
+			store,
+		})
+		expect(wrapper.text()).toMatch('this is the first item')
+		expect(wrapper.text()).toMatch('this is the second item')
+	})
+})
