8ea0f998b0db182fced8846442b3e9eb21106022
owlgrass <32409699+owlgrass@users.noreply.github.com>
2022-10-30 21:34:20 +0800

    Change tests to use mock store instead of real store

--- tests/unit/TodoView.spec.js
+++ tests/unit/TodoView.spec.js
@@ -1,7 +1,6 @@
 import Vuex from 'vuex'
 import { mount, createLocalVue } from '@vue/test-utils'
 import ElementUI from 'element-ui'
-import store from '@/store'
 import TodoView from '@/views/HomeView/TodoView.vue'
 
 const localVue = createLocalVue()
@@ -9,31 +8,38 @@ localVue.use(Vuex)
 localVue.use(ElementUI)
 
 describe('TodoView.vue', () => {
-	let wrapper
-	beforeEach(() => {
-		wrapper = mount(TodoView, {
-			localVue,
-			store,
-		})
-	})
-	afterEach(() => {
-		wrapper.destroy()
-	})
-
 	it('renders todo list with 0 items', () => {
+		const store = new Vuex.Store({
+			getters: {
+				todos() {
+					return []
+				},
+			},
+		})
+		const wrapper = mount(TodoView, { localVue, store })
+
 		expect(wrapper.text()).toMatch('No items')
 	})
 
-	it('renders todo list with 1 or more items', async () => {
-		store.dispatch('add', {
-			content: 'this is the first item',
-			checked: true,
+	it('renders todo list with 1 or more items', () => {
+		const store = new Vuex.Store({
+			getters: {
+				todos() {
+					return [
+						{
+							content: 'this is the first item',
+							checked: true,
+						},
+						{
+							content: 'this is the second item',
+							checked: true,
+						},
+					]
+				},
+			},
 		})
-		store.dispatch('add', {
-			content: 'this is the second item',
-			checked: true,
-		})
-		await wrapper.vm.$nextTick()
+		const wrapper = mount(TodoView, { localVue, store })
+
 		expect(wrapper.text()).toMatch('this is the first item')
 		expect(wrapper.text()).toMatch('this is the second item')
 	})
