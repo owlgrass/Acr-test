8dbaaa0843ca8e1a3e90f7e8cb766855b87d21a1
owlgrass <32409699+owlgrass@users.noreply.github.com>
2022-10-30 19:59:55 +0800

    Add element-ui to unit tests
    This prevents console warnings

--- tests/unit/TodoView.spec.js
+++ tests/unit/TodoView.spec.js
@@ -1,14 +1,17 @@
 import Vuex from 'vuex'
 import { mount, createLocalVue } from '@vue/test-utils'
+import ElementUI from 'element-ui'
 import store from '@/store'
 import TodoView from '@/views/HomeView/TodoView.vue'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
+localVue.use(ElementUI)
 
 describe('TodoView.vue', () => {
 	it('renders todo list with 0 items', () => {
 		const wrapper = mount(TodoView, {
+			localVue,
 			store,
 		})
 		expect(wrapper.text()).toMatch('No items')
@@ -24,6 +27,7 @@ describe('TodoView.vue', () => {
 			checked: true,
 		})
 		const wrapper = mount(TodoView, {
+			localVue,
 			store,
 		})
 		expect(wrapper.text()).toMatch('this is the first item')
