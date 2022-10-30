f37cf2a656a266c635f3b0b0d57d7d76c5910a3e
owlgrass <32409699+owlgrass@users.noreply.github.com>
2022-10-30 22:06:27 +0800

    Error validation for empty todos

--- src/views/HomeView/TodoView/TodoAdd.vue
+++ src/views/HomeView/TodoView/TodoAdd.vue
@@ -1,10 +1,13 @@
 <template>
 	<div class="todo-add">
-		<el-input
-			placeholder="Enter a todo item..."
-			v-model="input"
-			@keydown.native.enter="() => add()"
-		/>
+		<span class="input">
+			<el-input
+				placeholder="Enter a todo item..."
+				v-model="input"
+				@keydown.native.enter="() => add()"
+			/>
+			<div class="error-message">{{ errorMessage }}</div>
+		</span>
 		<el-button type="primary" @click="add">Add</el-button>
 	</div>
 </template>
@@ -17,15 +20,26 @@ export default {
 			 * @type {String}
 			 */
 			input: '',
+			errorMessage: '',
 		}
 	},
 	methods: {
 		add() {
+			if (this.input === '') {
+				this.error()
+				return
+			}
 			this.$emit('add', {
 				content: this.input,
 				checked: false,
 			})
 			this.input = ''
+		},
+		error() {
+			this.errorMessage = '* This cannot be empty'
+			setTimeout(() => {
+				this.errorMessage = ''
+			}, 1000)
 		},
 	},
 	components: {},
@@ -35,15 +49,23 @@ export default {
 <style scoped lang="scss">
 @import '@/theme.scss';
 
-.el-input {
+.input {
 	margin-right: 1em;
 
+	flex-grow: 1;
+}
+.el-input {
 	&::v-deep > .el-input__inner {
 		border: 1px solid $--color-secondary;
 	}
 }
+.error-message {
+	color: $--color-danger;
+	font-size: 0.8em;
+}
 .todo-add {
 	display: flex;
+	align-items: flex-start;
 
 	padding: 1em;
 	margin-bottom: -1px;
